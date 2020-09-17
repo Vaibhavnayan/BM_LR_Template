6e333724eacaae101b65e6f547ef47578949fb5d
Vaibhav Nayan <vaibhav.16.nayan@gmail.com>
2020-09-17 12:19:54 +0530

    first commit

--- TxnNaming_API.py
+++ TxnNaming_API.py
@@ -1,10 +1,15 @@
 import time #built-in libraries-  written inside pyton interpreter in C langauge- import sys sys.builtin_module_names
 import os #standard libraries- written in both C in python, reside in python libraries. import sys sys.prefix
 import pandas #third party libraries- written by third party- to install pip3 install pandas
+import email, smtplib, ssl
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 def addStartContent(index,contents,txnName,textCheck,value,newFilename):
     #contents.pop(index)
-    contents.insert(index, "web_reg_find(\"Text=%s\", \"SaveCount=TextCount\", LAST); \n lr_start_transaction(\"%s\");\n " %(textCheck[value],txnName[value]))
+    contents.insert(index, "web_reg_find(\"Text=%s\", \"SaveCount=TextCount_%s\", LAST); \n lr_start_transaction(\"%s\");\n " %(textCheck[value],value,txnName[value]))
     
     f = open(newFilename, "w")
     contents = "".join(contents)
@@ -15,7 +20,7 @@ def addStartContent(index,contents,txnName,textCheck,value,newFilename):
 
 def addEndContent(index2,contents,txnName,textCheck,value,fileName):
 
-    contents.insert(index2+1, "\n if(atoi(lr_eval_string(\"{%s}\")) > = 0){ \n lr_end_transaction(\"%s\",LR_PASS);\n} \n else { \n lr_end_transaction(\"%s\",LR_FAIL);\n}\n lr_think_time(2);" %(textCheck[value],txnName[value],txnName[value]))
+    contents.insert(index2+1, "\n if(atoi(lr_eval_string(\"{TextCount_%s}\")) > = 0){ \n lr_end_transaction(\"%s\",LR_PASS);\n} \n else { \n lr_end_transaction(\"%s\",LR_FAIL);\n}\n lr_think_time(2);" %(value,txnName[value],txnName[value]))
    # contents.pop(index2)
         
     f = open(fileName, "w")
@@ -75,6 +80,7 @@ def datasheet(sheetPath):
 
 
 def mainFunc(oldfilePath,newfilePath,excelPath):
+    global newFilename
     fileName = oldfilePath.filename
    # filedata= oldfilePath.read()
     newFilename = newfilePath
@@ -94,9 +100,72 @@ def mainFunc(oldfilePath,newfilePath,excelPath):
         #print(textCheck)
         openFileforStartTxn(0,fileName,txnName,textCheck,newFilename)
         openFileforEndTxn(0,newFilename,txnName,textCheck)
-        f = open(newFilename,'r')
-        data = f.read()
-        f.close()
-        return "Done", newFilename, data
+        # f = open(newFilename,'r')
+        # data = f.read()
+        # f.close()
+        return "Done", newFilename, "Done"
     else:
         return "File path doesn't exists", newFilename, sheetpath
+
+
+def download():
+    f = open("{}".format(newFilename),'r')
+    data2 = f.read()
+    f.close()
+    return newFilename,data2
+
+def sendMail(to,fromEmail,pwd,sub,message):
+    subject = sub
+    body = message
+    sender_email = fromEmail
+    receiver_email = to
+    password = pwd
+    # Create a multipart message and set headers
+    try:
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
+        message["Bcc"] = receiver_email  # Recommended for mass emails
+
+        # Add body to email
+        message.attach(MIMEText(body, "plain"))
+
+        filename = newFilename  # In same directory as script
+
+        # Open PDF file in binary mode
+        with open(filename, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+        # Encode file in ASCII characters to send by email    
+        encoders.encode_base64(part)
+
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {filename}",
+        )
+
+        # Add attachment to message and convert message to string
+        message.attach(part)
+        text = message.as_string()
+
+        # Log in to server using secure context and send email
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, text)
+        
+        return "sent",filename
+    
+    except smtplib.SMTPAuthenticationError as e:
+        return "Error",e
+    
+    except TypeError as er:
+        return "Error",er
+    
+    except KeyError as err:
+        return "Error",err
